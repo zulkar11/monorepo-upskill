45ce8d7500b579c23d93504fc92d3e4992cb83ec
Zulkar Nayin <zulkar.nayin@bjitgroup.com>
2023-06-07 17:01:49 +0600

    complete up skill assingment

---
+++ libs/component/src/lib/component.tsx
@@ -0,0 +1,14 @@
+import styles from './component.module.scss';
+
+/* eslint-disable-next-line */
+export interface ComponentProps {}
+
+export function Component(props: ComponentProps) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Component!</h1>
+    </div>
+  );
+}
+
+export default Component;
